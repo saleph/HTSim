a781ed203797a52fccd067ef7233c6392cafce68
Tomasz Galecki <saleph@protonmail.com>
2018-04-06 09:34:17 +0200

    add skeleton

---
+++ batch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python2
+# Simple example of batch program. Takes a file as argv.
+import sys
+import time
+
+
+def do_job():
+    if len(sys.argv) != 2:
+        raise AttributeError
+    numbers = []
+    with open(sys.argv[1]) as f:
+        for line in f:
+            numbers.append(float(line))
+    powers = [x**2 for x in numbers]
+
+    time.sleep(3)
+
+    with open("powers", "w") as f:
+        for a, b in zip(numbers, powers):
+            f.write("{}^2={}".format(a, b))
+            f.write("\n")
+
+
+if __name__ == "__main__":
+    do_job()
+    print "elo job"
